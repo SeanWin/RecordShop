ee216f5e203b122aea39fe3dd43c0c6626691baa
Sean Winston <seanwinston@gmail.com>
2024-12-05 11:18:05 +0000

    feat: add controller layer with base path

---
+++ src/main/java/com/northcoders/RecordShop/controller/RecordshopController.java
@@ -0,0 +1,9 @@
+package com.northcoders.RecordShop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/recordshop")
+public class RecordshopController {
+}

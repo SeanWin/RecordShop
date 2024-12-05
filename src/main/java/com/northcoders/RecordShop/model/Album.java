efdd68d42e698feeb6ed8bdc76ae9823f76c274f
Sean Winston <seanwinston@gmail.com>
2024-12-05 11:01:57 +0000

    feat: add album and genre data models and add required dependencies to pom

---
+++ src/main/java/com/northcoders/RecordShop/model/Album.java
@@ -0,0 +1,47 @@
+package com.northcoders.RecordShop.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(nullable = false)
+    private String artist;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Genre genre;
+
+    @NotNull
+    @Column(nullable = false)
+    private int releaseYear;
+
+    @NotNull
+    @Column(nullable = false)
+    private int stockCount;
+
+    @NotNull
+    @Column(nullable = false)
+    private double price;
+}
---
+++ src/main/java/com/northcoders/RecordShop/model/Genre.java
@@ -0,0 +1,14 @@
+package com.northcoders.RecordShop.model;
+
+public enum Genre {
+    ROCK,
+    POP,
+    JAZZ,
+    CLASSICAL,
+    HIPHOP,
+    ELECTRONIC,
+    COUNTRY,
+    REGGAE,
+    BLUES,
+    OTHER
+}

a50af0c76b892cb3fb8aa7d3f03f5270b4a62b45
Sean Winston <seanwinston@gmail.com>
2024-12-09 00:19:14 +0000

    feat: create and update Album method interact with AlbumDTO rather than directly with the Album

--- src/main/java/com/northcoders/RecordShop/controller/AlbumController.java
+++ src/main/java/com/northcoders/RecordShop/controller/AlbumController.java
@@ -1,6 +1,7 @@
 package com.northcoders.RecordShop.controller;
 
 import com.northcoders.RecordShop.model.Album;
+import com.northcoders.RecordShop.model.AlbumDTO;
 import com.northcoders.RecordShop.service.AlbumService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,14 +34,14 @@ public class AlbumController {
 
 
     @PostMapping
-    public ResponseEntity<Album> createAlbum(@Valid @RequestBody Album album){
-        Album newAlbum = albumService.insertAlbum(album);
+    public ResponseEntity<Album> createAlbum(@RequestBody @Valid AlbumDTO albumDTO){
+        Album newAlbum = albumService.insertAlbum(albumDTO.toAlbum());
         return new ResponseEntity<>(newAlbum, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Album> updateAlbum(@PathVariable("id") long id, @RequestBody Album album){
-        Album updatedAlbum = albumService.updateAlbumById(id, album);
+    public ResponseEntity<Album> updateAlbum(@PathVariable("id") long id, @RequestBody @Valid AlbumDTO albumDTO){
+        Album updatedAlbum = albumService.updateAlbumById(id, albumDTO.toAlbum());
         return new ResponseEntity<>(updatedAlbum, HttpStatus.OK);
 
     }

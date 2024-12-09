835206480c3066c2af9e939439fc5a22df1f491e
Sean Winston <seanwinston@gmail.com>
2024-12-09 09:51:28 +0000

    feat: add methods to handle MethodArgumentNotValid and httpMessageNotReadable exceptions to improve feedback for an invalid post/put

--- src/main/java/com/northcoders/RecordShop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/northcoders/RecordShop/exception/GlobalExceptionHandler.java
@@ -1,15 +1,47 @@
 package com.northcoders.RecordShop.exception;
 
+import com.northcoders.RecordShop.model.Genre;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<Object> handleAlbumFoundException(AlbumNotFoundException e) {
+    public ResponseEntity<Object> handleAlbumNotFoundException(AlbumNotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, List<String>> body = new HashMap<>();
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        String errorMessage = "Invalid Genre. Allowed values are: " + Genre.getAllowedValues();
+        Map<String, String> response = new HashMap<>();
+        response.put("error", errorMessage);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }

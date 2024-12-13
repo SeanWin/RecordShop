da41b8bc7db7c9cf48723f9382b8fc0f21a123a4
Sean Winston <seanwinston@gmail.com>
2024-12-13 11:36:06 +0000

    feat: create search package with AlbumSearchParameters and AlbumSearchSpecification with a toPredicate method which constructs a criteria based predicate for dynamic queries

---
+++ src/main/java/com/northcoders/RecordShop/search/AlbumSearchParameters.java
@@ -0,0 +1,43 @@
+package com.northcoders.RecordShop.search;
+
+import com.northcoders.RecordShop.model.Genre;
+
+public class AlbumSearchParameters {
+    private String artistName;
+    private Integer releaseYear;
+    private Genre genre;
+    private String albumName;
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+}
+
---
+++ src/main/java/com/northcoders/RecordShop/search/AlbumSearchSpecification.java
@@ -0,0 +1,42 @@
+package com.northcoders.RecordShop.search;
+
+import com.northcoders.RecordShop.model.Album;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlbumSearchSpecification implements Specification<Album> {
+
+    private final AlbumSearchParameters params;
+
+    public AlbumSearchSpecification(AlbumSearchParameters params) {
+        this.params = params;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<Album> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (params.getArtistName() != null && !params.getArtistName().isEmpty()) {
+            predicates.add( builder.like(builder.lower(root.get("artist").get("name")),
+                    "%" + params.getArtistName().toLowerCase() + "%"));
+        }
+        if (params.getReleaseYear() != null) {
+            predicates.add(builder.equal(builder.function("YEAR", Integer.class, root.get("releaseDate")), params.getReleaseYear()));
+        }
+        if (params.getGenre() != null) {
+            predicates.add(builder.equal(root.get("genre"), params.getGenre()));
+        }
+        if (params.getAlbumName() != null && !params.getAlbumName().isEmpty()) {
+            predicates.add(builder.like(builder.lower(root.get("name")),
+                    "%" + params.getAlbumName().toLowerCase() + "%"));
+        }
+
+        return builder.and(predicates.toArray(new Predicate[0]));
+    }
+}

e7f554fcf9e5afdb6906cc45454f01d3f4d1620b
Sean Winston <seanwinston@gmail.com>
2024-12-28 23:52:39 +0000

    feat: delombok model class due to issue with mvn package

--- src/main/java/com/northcoders/RecordShop/model/Artist.java
+++ src/main/java/com/northcoders/RecordShop/model/Artist.java
@@ -2,33 +2,65 @@ package com.northcoders.RecordShop.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 import java.util.List;
 
 @Table(name="artists")
 @Entity
-@Data
-@Accessors(chain = true)
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Artist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    private Long id;
 
     @Column(nullable = false)
-    public String name;
+    private String name;
 
     @Column(nullable = false)
-    public String nationality;
+    private String nationality;
 
     @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
     @JsonBackReference
     private List<Album> albums;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<Album> albums) {
+        this.albums = albums;
+    }
+
+    public Artist() {
+    }
+
+    public Artist(Long id, String name, String nationality, List<Album> albums) {
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+        this.albums = albums;
+    }
 }
